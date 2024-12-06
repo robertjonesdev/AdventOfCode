4fade4a1db5562322663f9516cdf2f5daa625409
robertjonesdev <rtjones06@gmail.com>
2024-12-06 13:44:16 -0700

    2024/6

--- 2024/6.py
+++ 2024/6.py
@@ -17,11 +17,27 @@ TEST_DATA = """....#.....
 ......#...
 """
 
+# Globals
+right_bound = 0
+down_bound = 0
+game_board = None
+start_pos = (-1, -1)
+
 
 def parse(data: str):
     """Parse input."""
+    global game_board
+    game_board = [list(line) for line in data.splitlines()]
 
-    return [list(line) for line in data.splitlines()]
+    global right_bound
+    right_bound = len(game_board[0])
+
+    global down_bound
+    down_bound = len(game_board)
+
+    global start_pos
+    start_pos = find_start(game_board)
+    return
 
 
 def find_start(data):
@@ -31,69 +47,67 @@ def find_start(data):
                 return row_idx, col_idx
 
 
-def part1(data):
+# Up-0, Right-1, Down-2, Left-3
+directions = {0: (-1, 0), 1: (0, 1), 2: (1, 0), 3: (0, -1)}
+
+
+def turn_right(direction):
+    return (direction + 1) % 4
+
+
+def inbounds(x, y):
+    return 0 <= x < down_bound and 0 <= y < right_bound
+
+
+def traverse_map(obstacle=None):
+
+    row_pos, col_pos = start_pos
+    direction = 0
+    visited = set()
+    visited_direction = set()
+
+    while inbounds(row_pos, col_pos):
+        visited.add((row_pos, col_pos))
+
+        d_row, d_col = directions[direction]
+        n_row, n_col = row_pos + d_row, col_pos + d_col
+
+        if inbounds(n_row, n_col) and (
+            game_board[n_row][n_col] == "#" or (obstacle and (n_row, n_col) == obstacle)
+        ):
+            direction = turn_right(direction)
+        else:
+            row_pos, col_pos = n_row, n_col
+
+        visited_with_direction = (row_pos, col_pos, direction)
+        if visited_with_direction in visited_direction:
+            return True, visited
+        visited_direction.add(visited_with_direction)
+
+    return False, visited
+
+
+def part1():
     """Solve part 1."""
-    right_bound = len(data[0]) - 1
-    down_bound = len(data) - 1
-    left_bound = 0
-    up_bound = 0
-
-    row_pos, col_pos = find_start(data)
-
-    direction = "UP"
-    continue_flag = True
-    while continue_flag:
-        data[row_pos][col_pos] = 1
-        if direction == "UP":
-            if row_pos > up_bound:
-                row_pos -= 1
-            else:
-                continue_flag = False
-
-            if data[row_pos][col_pos] == "#":
-                direction = "RIGHT"
-                row_pos += 1
-
-        elif direction == "DOWN":
-            if row_pos < down_bound:
-                row_pos += 1
-            else:
-                continue_flag = False
-            if data[row_pos][col_pos] == "#":
-                direction = "LEFT"
-                row_pos -= 1
-
-        elif direction == "RIGHT":
-            if col_pos < right_bound:
-                col_pos += 1
-            else:
-                continue_flag = False
-            if data[row_pos][col_pos] == "#":
-                direction = "DOWN"
-                col_pos -= 1
-
-        elif direction == "LEFT":
-            if col_pos > left_bound:
-                col_pos -= 1
-            else:
-                continue_flag = False
-            if data[row_pos][col_pos] == "#":
-                direction = "UP"
-                col_pos += 1
-
-    return sum(visited for row in data for visited in row if isinstance(visited, int))
+    return traverse_map()[1]
 
 
-def part2(data):
+def part2(visited):
     """Solve part 2."""
-    pass
+    obstacle_count = 0
+    obstacles = visited - {start_pos}
+    for obstacle in obstacles:
+        if traverse_map(obstacle)[0]:
+            obstacle_count += 1
+    return obstacle_count
 
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
-    data = parse(puzzle_input)
-    solution1 = part1(data)
-    solution2 = part2(data)
+    parse(puzzle_input)
+    visited = part1()
+    solution1 = len(visited)
+    solution2 = part2(visited)
 
     return solution1, solution2
 
